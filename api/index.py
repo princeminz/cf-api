6b90df02bd8edba48c227c416c4d4f473e8d1ff9
Samarpit Minz <samarpitminz@gmail.com>
2021-09-15 13:48:13 +0530

    some improvements

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ class handler(BaseHTTPRequestHandler):
         res = requests.get('https://codeforces.com/api/user.ratedList')
         self.send_response(200)
         self.send_header('Content-type','text/plain')
-        self.send_header('Cache-Control', 's-maxage=3600, stale-while-revalidate');
+        self.send_header('Cache-Control', 's-maxage=3600')
         self.end_headers()
         self.wfile.write(json.dumps({ user['handle']:user['organization'] for user in res.json()['result'] if 'organization' in user}).encode(encoding='utf_8'))
         return
