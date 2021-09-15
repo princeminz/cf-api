746e4bbae7af011d348de3b7c71e45d9b1b20e76
Samarpit Minz <samarpitminz@gmail.com>
2021-09-15 17:39:44 +0530

    add cloudflare as cache

--- api/index.py
+++ api/index.py
@@ -5,10 +5,10 @@ import json
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        res = requests.get('https://codeforces.com/api/user.ratedList')
+        res = requests.get('https://cf.minz.workers.dev')
         self.send_response(200)
         self.send_header('Content-type','text/plain')
-        self.send_header('Cache-Control', 's-maxage=3600')
+        self.send_header('Cache-Control', 's-maxage=3600, stale-while-revalidate')
         self.end_headers()
         self.wfile.write(json.dumps({ user['handle']:user['organization'] for user in res.json()['result'] if 'organization' in user}).encode(encoding='utf_8'))
         return
