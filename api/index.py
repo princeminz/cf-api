b132eb922c544da12e8e2c83e95a71df1fd0203c
Samarpit Minz <samarpitminz@gmail.com>
2021-09-17 22:47:48 +0530

    added rank

--- api/index.py
+++ api/index.py
@@ -12,20 +12,30 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Cache-Control', 's-maxage=3600, stale-while-revalidate')
         self.end_headers()
         params = parse_qs(self.path[5:])
-        if 'contest' in params and 'country' not in params:
+        if 'contest' in params:
             cres = requests.get('https://codeforces.com/api/contest.standings?contestId=' + params['contest'][0])
+            if 'country' in params:
+                user_country = { user['handle']:user['country'] for user in res.json()['result'] if 'country' in user}
+                user_country_contest = { record['party']['members'][0]['handle']:user_country[record['party']['members'][0]['handle']] for record in cres.json()['result']['rows'] if record['party']['members'][0]['handle'] in user_country}
+                self.wfile.write(json.dumps(user_country_contest).encode(encoding='utf_8'))
+                return
+            if 'rank' in params:
+                user_rank = { user['handle']:user['rank'] for user in res.json()['result']}
+                user_rank_contest = { record['party']['members'][0]['handle']:user_rank[record['party']['members'][0]['handle']] for record in cres.json()['result']['rows']}
+                self.wfile.write(json.dumps(user_rank_contest).encode(encoding='utf_8'))
+                return
             user_org = { user['handle']:user['organization'] for user in res.json()['result'] if 'organization' in user}
             user_org_contest = { record['party']['members'][0]['handle']:user_org[record['party']['members'][0]['handle']] for record in cres.json()['result']['rows'] if record['party']['members'][0]['handle'] in user_org}
             self.wfile.write(json.dumps(user_org_contest).encode(encoding='utf_8'))
-        elif 'contest' in params:
-            cres = requests.get('https://codeforces.com/api/contest.standings?contestId=' + params['contest'][0])
-            user_country = { user['handle']:user['country'] for user in res.json()['result'] if 'country' in user}
-            user_country_contest = { record['party']['members'][0]['handle']:user_country[record['party']['members'][0]['handle']] for record in cres.json()['result']['rows'] if record['party']['members'][0]['handle'] in user_country}
-            self.wfile.write(json.dumps(user_country_contest).encode(encoding='utf_8'))
-        elif 'country' in params:
-            user_country = { user['handle']:user['country'] for user in res.json()['result'] if 'country' in user}
-            self.wfile.write(json.dumps(user_country).encode(encoding='utf_8'))
         else:
+            if 'country' in params:
+                user_country = { user['handle']:user['country'] for user in res.json()['result'] if 'country' in user}
+                self.wfile.write(json.dumps(user_country).encode(encoding='utf_8'))
+                return
+            if 'rank' in params:
+                user_rank = { user['handle']:user['rank'] for user in res.json()['result']}
+                self.wfile.write(json.dumps(user_rank).encode(encoding='utf_8'))
+                return
             user_org = { user['handle']:user['organization'] for user in res.json()['result'] if 'organization' in user}
             self.wfile.write(json.dumps(user_org).encode(encoding='utf_8'))
         return
